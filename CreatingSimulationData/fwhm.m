eaed779eeb4a9b05c36af4c95c8ab9f34ae8e891
Cestlab_Zu <126023075+CESTlabZu@users.noreply.github.com>
2023-10-26 17:15:11 -0500

    Delete CreatingSimulationData directory

--- CreatingSimulationData/fwhm.m
+++
@@ -1,6 +0,0 @@
-function theWidth = fwhm(signal,k)
-    peakHeight = signal(16);
-    x1 = find(signal >= 0.5*peakHeight, 1, 'first');
-    x2 = find(signal >= 0.5*peakHeight, 1, 'last');
-    theWidth = abs(k(x2)-k(x1)); 
-end
--- CreatingSimulationData/getsatpulse.m
+++
@@ -1,5 +0,0 @@
-function satpulse = getsatpulse(curve, angle, t, duration, TR)
-num = 360*sum(curve)*duration/(256*angle*2*pi); 
-satpulse = curve./num;
-satpulse = satpulse(t);
-end
